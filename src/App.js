75d720f5aa71f8f21d4ee5e907f61aa977e479c0
BriWidowski <33921803+briwidowski@users.noreply.github.com>
2022-06-28 14:30:18 -0500

    Add operator to calculation only if conditions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
 function App() {
+  const [calc, setCalc] = useState("")
+  const [result, setResult] = useState("")
+
+  const operators = ['/', '*', '-', '+', '=', '.']
+
+  const updateCalc = (value) => {
+
+    if (
+      operators.includes(value) && result === "" ||
+      operators.includes(value) && operators.includes(calc.slice(-1))
+    ) {
+      return
+    }
+
+    setCalc(calc + value)
+
+    setResult(calc)
+  }
+
+  const clearOutput = () => {
+    setCalc("")
+    setResult("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="output">
+        { result ? <p>{calc}</p> : ''}
+        { calc || "0"}
+      </div>
+      <div className="buttons-grid">
+        <button onClick={() => clearOutput()} className="span-2">AC</button>
+        <button>DEL</button>
+        <button onClick={() => updateCalc("/")}>÷</button>
+        <button onClick={() => updateCalc("7")}>7</button>
+        <button onClick={() => updateCalc("8")}>8</button>
+        <button onClick={() => updateCalc("9")}>9</button>
+        <button onClick={() => updateCalc("*")}>*</button>
+        <button onClick={() => updateCalc("4")}>4</button>
+        <button onClick={() => updateCalc("5")}>5</button>
+        <button onClick={() => updateCalc("6")}>6</button>
+        <button onClick={() => updateCalc("-")}>-</button>
+        <button onClick={() => updateCalc("1")}>1</button>
+        <button onClick={() => updateCalc("2")}>2</button>
+        <button onClick={() => updateCalc("3")}>3</button>
+        <button onClick={() => updateCalc("+")}>+</button>
+        <button onClick={() => updateCalc("0")} className="span-2">0</button>
+        <button onClick={() => updateCalc(".")}>.</button>
+        <button>=</button>
+      </div>
     </div>
   );
 }
